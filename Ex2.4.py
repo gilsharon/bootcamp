164116b8c988d5e01a8660413ee405fab75767a6
Gil Sharon <gilsharon@gmail.com>
2017-06-21 09:41:00 -0700

    Ex2.4

--- Ex2.4.py
+++ Ex2.4.py
@@ -1,20 +1,69 @@
+import bioinfo_dicts as bd
+
+def find_next_start(seq):
+    '''
+    returns the first ATG index
+    '''
+    return seq.find('ATG')
+
+def find_first_stop(seq):
+    '''
+    input is a sequence starting after an ATG
+    returns the inframe stop conon ORF
+    '''
+    stop = ['TGA','TAG','TAA']
+    sequence = seq[:]
+    next_codon = sequence[:3]
+    sequence = sequence[3:]
+    ORF = ''
+    while next_codon not in stop and sequence != '':
+        ORF += next_codon
+        next_codon = sequence[:3]
+        sequence = sequence[3:]
+    if next_codon in stop:
+        return ORF + next_codon
+    else: return ''
+
 def longest_orf(seq):
     '''
     find the longest ORF in seq
     '''
-    sub_seq = seq.upper()
-    possible_ATG = []
-    # find possible start sites
-    while 'ATG' in sub_seq:
-        if possible_ATG != []:
-            last_ATG = possible_ATG[-1]+3
-            possible_ATG += [sub_seq.find('ATG') + last_ATG]
-            print(possible_ATG)
-        else:
-            possible_ATG += [sub_seq.find('ATG')]
-            print(possible_ATG)
-        print('***', sub_seq)
-        sub_seq = sub_seq[possible_ATG[-1]+3:]
-        print(sub_seq)
+    longest_orf = ''
+    current_index = find_next_start(seq)
+    sequence = seq[:]
+    while current_index != -1:
+        ORF = find_first_stop(sequence[current_index:])
+        sequence = sequence[3:]
+        current_index = find_next_start(sequence)
+        if len(ORF) > len(longest_orf):
+            longest_orf = ORF
+    return longest_orf
 
-    return possible_ATG
+import os
+
+filename = 'salmonella_spi1_region.fna'
+
+def read_fasta(filename):
+    '''
+    read fasta file and store in a single string
+    '''
+    fasta = ''
+    if os.path.isfile(filename):
+        with open(filename, 'r') as f:
+            header = f.readline()
+            fasta_file = f.readlines()
+            for i, line in enumerate(fasta_file):
+                    fasta += line.rstrip()
+    else:
+        raise RuntimeError("{} does not exist".format(filename))
+    return fasta
+
+def dna2protein(orf):
+    '''
+    convert DNA seq to protein seq
+    '''
+    orf_protein = ''
+    for i in list(range(int(len(orf)/3))):
+        current_codon = orf[i*3:(i+1)*3]
+        orf_protein += bd.codons[current_codon]
+    return orf_protein
