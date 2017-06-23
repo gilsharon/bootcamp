ffb9222c8a69fe33af314590970ff1290151ef9e
Gil Sharon <gilsharon@gmail.com>
2017-06-23 10:53:36 -0700

    PANDAs tutorial

--- frogs.py
+++ frogs.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #load data
-df = pd.read_csv('data/frog_tongue_adhesion.csv', comment='#')
 
 # group by frog ID.
 imp_force_df = df.loc[:,['ID', 'impact force (mN)']]
