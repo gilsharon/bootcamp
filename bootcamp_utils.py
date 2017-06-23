cc5b33a0a2ee3f7295e8872d2b1c30475b480fbc
Gil Sharon <gilsharon@gmail.com>
2017-06-23 10:54:22 -0700

    Test Driven Development

--- bootcamp_utils.py
+++ bootcamp_utils.py
@@ -8,17 +8,6 @@ colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
           '#bcbd22', '#17becf']
 sns.set(style='whitegrid', palette=colors, rc={'axes.labelsize': 16})
 
-
-def ecdf(data):
-    '''
-    return X and y values for ECDF plotting
-    '''
-
-    x = np.sort(data)
-    y = np.arange(1,len(data)+1)/ len(data)
-
-    return x, y
-
 def bs_replicate(data, func = np.mean):
     '''
     compute a bootstrap replicate from data for a function func
@@ -32,3 +21,106 @@ def draw_bs_reps(data, func=np.mean, size=10000):
     draw bootstrap replicates from 1D data
     '''
     return np.array([bs_replicate(data, func=func) for _ in range(size)])
+
+def ecdf(data, formal=False, buff=0.1, min_x=None, max_x=None):
+    """
+    Generate `x` and `y` values for plotting an ECDF.
+
+    Parameters
+    ----------
+    data : array_like
+        Array of data to be plotted as an ECDF.
+    formal : bool, default False
+        If True, generate `x` and `y` values for formal ECDF.
+        Otherwise, generate `x` and `y` values for "dot" style ECDF.
+    buff : float, default 0.1
+        How long the tails at y = 0 and y = 1 should extend as a
+        fraction of the total range of the data. Ignored if
+        `formal` is False.
+    min_x : float, default None
+        Minimum value of `x` to include on plot. Overrides `buff`.
+        Ignored if `formal` is False.
+    max_x : float, default None
+        Maximum value of `x` to include on plot. Overrides `buff`.
+        Ignored if `formal` is False.
+
+    Returns
+    -------
+    x : array
+        `x` values for plotting
+    y : array
+        `y` values for plotting
+    """
+
+    if formal:
+        return _ecdf_formal(data, buff=buff, min_x=min_x, max_x=max_x)
+    else:
+        return _ecdf_dots(data)
+
+def _ecdf_dots(data):
+    """
+    Compute `x` and `y` values for plotting an ECDF.
+
+    Parameters
+    ----------
+    data : array_like
+        Array of data to be plotted as an ECDF.
+
+    Returns
+    -------
+    x : array
+        `x` values for plotting
+    y : array
+        `y` values for plotting
+    """
+    return np.sort(data), np.arange(1, len(data)+1) / len(data)
+
+def _ecdf_formal(data, buff=0.1, min_x=None, max_x=None):
+    """
+    Generate `x` and `y` values for plotting a formal ECDF.
+
+    Parameters
+    ----------
+    data : array_like
+        Array of data to be plotted as an ECDF.
+    buff : float, default 0.1
+        How long the tails at y = 0 and y = 1 should extend as a fraction
+        of the total range of the data.
+    min_x : float, default None
+        Minimum value of `x` to include on plot. Overrides `buff`.
+    max_x : float, default None
+        Maximum value of `x` to include on plot. Overrides `buff`.
+
+    Returns
+    -------
+    x : array
+        `x` values for plotting
+    y : array
+        `y` values for plotting
+    """
+    # Get x and y values for data points
+    x, y = _ecdf_dots(data)
+
+    # Set defaults for min and max tails
+    if min_x is None:
+        min_x = x[0] - (x[-1] - x[0])*buff
+    if max_x is None:
+        max_x = x[-1] + (x[-1] - x[0])*buff
+
+    # Set up output arrays
+    x_formal = np.empty(2*(len(x) + 1))
+    y_formal = np.empty(2*(len(x) + 1))
+
+    # y-values for steps
+    y_formal[:2] = 0
+    y_formal[2::2] = y
+    y_formal[3::2] = y
+
+    # x- values for steps
+    x_formal[0] = min_x
+    x_formal[1] = x[0]
+    x_formal[2::2] = x
+    x_formal[3:-1:2] = x[1:]
+    x_formal[-1] = max_x
+
+    return x_formal, y_formal
---
+++ seq_features.py
@@ -0,0 +1,26 @@
+import bioinfo_dicts
+
+def n_neg(seq):
+    """Number of negative residues a protein sequence"""
+
+    # Convert sequence to upper case
+    seq = seq.upper()
+
+    # Make sure AAs are valid
+    for aa in seq:
+        if aa not in bioinfo_dicts.aa.keys():
+            raise RuntimeError('{} is not a valid amino acid'.format(seq))
+
+
+    # Count E's and D's, since these are the negative residues
+    return seq.count('E') + seq.count('D')
+
+def test_n_neg():
+    """Perform unit tests on n_neg."""
+
+    assert n_neg('E') == 1
+    assert n_neg('D') == 1
+    assert n_neg('') == 0
+    assert n_neg('ACKLWTTAE') == 1
+    assert n_neg('DDDDEEEE') == 8
+    assert n_neg('acklwttae') == 1
